2cec26b08c9317e816fd53f9b10dcfd0ffcfcae8
Matthieu FROM viopc <mathieu.helary@gmail.com>
2021-11-22 00:36:17 +0100

    ajout dossier erreur  + FS

--- delPdf.sh
+++ delPdf.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-DIRSRC=/mnt/c/Users/vio pro/Desktop/Apicrypt
-DIRSCANNER=$DIRSRC/SCANNER
-DIRDOUBLON=$DIRSRC/DOUBLON_PDF
+DIRSRC=apicryptweda2
+DIRARCHIVES=archives_apicrypt2
+DIRSCANNER=$DIRSRC/COURRIER_a_envoyer
+DIRDOUBLON=$DIRSRC/NE_PAS_ENVOYER/TXTissuPDF
+DIRERROR=$DIRSRC/erreur_a_traiter
+CURRENTDATE=$(date -I)
 echo "Verification des dossiers"
 if [ ! -d "$DIRSCANNER" ]; then
         echo "Creation du dossier scanner";
-        mkdir -p $DIRSCANNER
+        mkdir -p "$DIRSCANNER"
 fi
 if [ ! -d "$DIRDOUBLON" ]; then
         echo "Creation du dossier Doublons";
-        mkdir -p $DIRDOUBLON
+        mkdir -p "$DIRDOUBLON"
+fi
+if [ ! -d "$DIRERROR" ]; then
+        echo "Creation du dossier Erreurs";
+        mkdir -p "$DIRERROR"
 fi
 echo "Verification terminée --- Let's Go !!"
 read gobble
@@ -20,14 +27,14 @@ find ./ -name "* *" -exec rename 's/ /_/g' "{}" \;
 echo "Terminé"
 
 echo "Traitement des doublons Textes qui existent deja en PDF (Fichier extrait automatiquement d'un pdf)"
-LIST="$(grep -r -Ei 'automatiquement extrait depuis un document PDF' $DIRSRC/*.Txt | cut -c13- |sed -e "s/ /_/g" | sed -e "s/_matches//g")"
+LIST="$(grep -r -Ei 'automatiquement extrait depuis un document PDF' "$DIRSRC/*.Txt" | cut -c13- |sed -e "s/ /_/g" | sed -e "s/_matches//g")"
 
 for fichier in $LIST
 do
         BASE=$( basename $fichier )
         DIRSRC=$( dirname $fichier )
-        echo "Deplacement du fichier $BASE du repertoire $DIRSRC vers le répertoire $DIRSRC/DOUBLON_PDF/"
-        mv $fichier "$DIRSRC/DOUBLON_PDF/$BASE"
+        echo "Deplacement du fichier $BASE du repertoire $DIRSRC vers le répertoire $DIRDOUBLON/$CURRENTDATE"
+        mv $fichier "$DIRDOUBLON/$CURRENTDATE/$BASE"
 done
 echo "Terminé"
 
@@ -36,16 +43,16 @@ LIST="$(grep -r -Eil 'cher Confrere|chere Consœur|cher confrére|cher confrère
 for fichier in $LIST
 do
         FILENAME=$( basename $fichier )
-        echo "Deplacement du fichier $FILENAME vers $DIRSCANNER"
+        echo "Deplacement du fichier $FILENAME vers $DIRSCANNER/$CURRENTDATE"
         INIT=$(echo "$DIRSRC/$FILENAME" )
-        DEST=$(echo "$DIRSCANNER/$FILENAME" )
+        DEST=$(echo "$DIRSCANNER/$CURRENTDATE/$FILENAME" )
 #       echo "$INIT --- $DEST"
         mv $INIT $DEST
 done
 echo "Terminé"
 
 echo "Conversion des fichiers texte en PDF dans le dossier scanner"
-cd $DIRSCANNER
+cd $DIRSRC
 for FILE in $( ls *.Txt )
 do
         IFS=':'
@@ -59,7 +66,7 @@ do
                 rm $FILE $FILE.save
         else
                 echo "ERREUR dans le traitement du fichier $FILE"
-                mv $FILE.save $FILE.error
+                mv $FILE.save $DIRERROR/$FILE.error
                 rm $FILE
         fi
 
